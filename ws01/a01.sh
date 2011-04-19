7705a9a5f3084493eb95c214e9f2ff96b2d9aa60
Niklas Fischer <niklasfi@gmail.com>
2011-04-19 16:17:16 +0200

    a01 finished

--- ws01/a01.sh
+++ ws01/a01.sh
@@ -1,12 +1,20 @@
 DIR="training"
 
+ewc=`gunzip -c "$DIR/e" | wc -w`
+fwc=`gunzip -c "$DIR/f" | wc -w`
+elc=`gunzip -c "$DIR/e" | wc -l`
+flc=`gunzip -c "$DIR/f" | wc -l`
 echo "Anz lfd Wörter in e:"
-gunzip -c "$DIR/e" | wc -w
+echo $ewc
 echo "And lfd Wörter in f:"
-gunzip -c "$DIR/f" | wc -w
+echo $fwc
 
 echo "Größe des Vokabulars in e:"
 gunzip -c "$DIR/e" | sed "s/ /\n/g" | sort | uniq | wc -l
 echo "Größe des Vokabulars in f:"
 gunzip -c "$DIR/f" | sed "s/ /\n/g" | sort | uniq | wc -l
 
+echo "durchschnittliche Satzlänge e:"
+echo "$ewc/$elc" | bc -l
+echo "durchschnittliche Satzlänge f:"
+echo "$fwc/$flc" | bc -l
