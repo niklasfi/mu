65cd6d4a919618f0882fc233d19170933e15a24d
Niklas Fischer <niklasfi@gmail.com>
2011-04-24 19:38:08 +0200

    added tests

--- ws01/global.h
+++ ws01/global.h
@@ -1,3 +1,5 @@
 #pragma once
 
+#include <assert.h>
+
 typedef unsigned int uint;
---
+++ ws01/tests.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../global.h"
+#include "../lexicon.h"
+#include "../language.h"
+#include "../word.h"
+
+bool word();
+bool lexicon();
