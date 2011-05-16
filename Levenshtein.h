ec8f4a8bd8993cc7d84a6adacf6d9bce2afc28a0
Valentina <valen_gerber@yahoo.de>
2011-05-16 16:57:30 +0200

    Levenshtein.h gefixt und Konstruktor geschrieben

--- Levenshtein.h
+++ Levenshtein.h
@@ -1,12 +1,24 @@
+#include <vector>
+#include <iostream>
+#include "lexicon.h"
+
+
 class Levenshtein{
-	public:
+
+private:
+	int _height;
+	int _width;
+
+	int max(int i, int j);
+
+public:
 	unsigned int* data;
 	unsigned int** matrix;
-
-	Levenshtein(vector<int>* reference, vector<int>* guess);//die Referenzübersetzung und die Übersetzung in ID's sind 
-	~Levenshtein;
 	
-	void print();//gibt die nötigen Veränderungen von guess auf reference aus.
+	Levenshtein(std::vector<int>* reference, std::vector<int>* guess);//Eingabe sind die Referenzübersetzung und die zu bewertende Übersetzung, allerdings in ID's 
+	~Levenshtein();
+	
+	void print(Lexicon* elex);//gibt die nötigen Veränderungen von guess auf reference aus.
 
 	int distance(); //gibt die Levenshtein-Distance aus
-}
+};
