44ca9bd3c4b9cee2e10bb736f6c7a294b653ce4c
Niklas Fischer <niklasfi@gmail.com>
2011-05-22 20:55:38 +0200

    NOT STABLE! switching from laptop to pc.

--- bleu.cpp
+++ bleu.cpp
@@ -11,10 +11,7 @@ double bleu_bp(uint referenceSize, uint guessSize){
 	if(c >= r) return 1;
 	return exp(1-r/c);
 }
-
-double bleu(SentencePool& sp){
-	uint N = 4;
-
+double bleu(SentencePool& sp, uint N = 4){
 	uint* matchcount = new uint[N];
 	std::fill(matchcount, matchcount + N, 0);//alles auf 0 setzen
 	
@@ -22,28 +19,28 @@ double bleu(SentencePool& sp){
 	std::fill(gramcount, gramcount + N, 0);
 	
 	uint refsize = 0;
-	uint guesssize = 0;
+	uint hypsize = 0;
 	
 	for(int sent = 0; sent < sp.guess.size(); sent++){
-		std::vector<uint>& gs = *sp.guess[sent], ref = *sp.reference[sent];
+		std::vector<uint>& hyp = *sp.guess[sent], ref = *sp.reference[sent];
 		
-		char* guess_n = new char[gs.size()];
-		std::fill(guess_n, guess_n + gs.size(), 0);
+		char* gramsize = new char[ hyp.size()];
+		std::fill(gramsize, gramsize + hyp.size(), 0);
 		
 		for(int k = 0; k < ref.size(); k++){ //k index des vergleichwortes in der ref
-			for(int l = 0; l < gs.size(); l++){ //l index des wortes im Ü-Vorschlag
-				if(guess_n[l] < N){ //wenn wir für dieses wort schon ein n-gram maximaler länge gefunden haben, wissen wir, dass wir nicht weitersuchen müssen
+			for(int l = 0; l < hyp.size(); l++){ //l index des wortes im Ü-Vorschlag
+				if(gramsize[l] < N){ //wenn wir für dieses wort schon ein n-gram maximaler länge gefunden haben, wissen wir, dass wir nicht weitersuchen müssen
 					int seq = 0;
-					while(seq+k < ref.size() && seq + l < gs.size() && ref[seq+k] == gs[seq+l] && seq < N){
+					while(seq+k < ref.size() && seq + l < hyp.size() && ref[seq+k] == hyp[seq+l] && seq < N){
 						/*     (1)                     (2)                        (3)                (4)
 						 * (1) überprüfen, ob das n-gram noch in den Referenzsatz passt
 						 * (2) überprüfen, ob das n-gram noch in den Übersetzungsvorschlag passt
 						 * (3) überprüfen, ob das Wort im Rerfenz- mit dem Vorschlagssatz übereinstimmt
 						 * (4) abbrechen, wenn seq zu groß wird
 						 */
-						if(seq > guess_n[l]){
-							matchcount[guess_n[l]]++;
-							guess_n[l]++;
+						if(seq >= gramsize[l]){
+							matchcount[gramsize[l]]++;
+							gramsize[l]++;
 						}
 						seq++;
 					}
@@ -51,9 +48,9 @@ double bleu(SentencePool& sp){
 			}
 		}
 		refsize += ref.size();
-		guesssize += gs.size();
+		hypsize += hyp.size();
 		
-		for(int i = 0; i < N; i++)
+		for(int i = 0; i < N && ref.size()-i > 0; i++)
 			gramcount[i] += ref.size() - i;
 	}
 	
@@ -66,5 +63,5 @@ double bleu(SentencePool& sp){
 
 	result /= N;
 
-	return result * bleu_bp(refsize,guesssize);
+	return result * bleu_bp(refsize,hypsize);
 }
