eba06e2054bfd1d48976f2c03d54cf25dba03a45
Niklas Fischer <niklasfi@gmail.com>
2011-06-06 12:39:32 +0200

    translate sollte laufen ...

--- translate.cpp
+++ translate.cpp
@@ -18,37 +18,41 @@ int main (int argc, char* argv[]){
      
      PTree<PTree<double>> schwarz;
      
-     if(argc!=3){
-	  cout<<"usage: template.exe <phrasentabelle> <quelltext>\n";
-	  exit(1);
-     }
+		if(argc!=3){
+			cout<<"usage: template.exe <phrasentabelle> <quelltext>\n";
+			exit(1);
+		}
      
      igzstream in(argv[1]);
      string line,token;
      
      
      
-     while(getline(in,line)){ 
-	  stringstream ist(line);
-	  
-	  double relfreq;
-	  vector<uint> ephrase, fphrase;
-	  
-	  ist >> relfreq >> token >>token; // token für "#" ACHTUNG das zweite Einlesen ist nur für die alten Trainingsdaten,da gibts relfreq zweimal
-	  
-	  while(ist>>token && token != "#"){
-	      fphrase.push_back(flex.getWord_or_add(token));
-	  }
-	  while(ist>>token){
-	      ephrase.push_back(elex.getWord_or_add(token));
-	  }
-	  
-	  PTree<double>* blau=new PTree<double>(); //wir alloquieren speicher für den blauBaum 
-	  blau->traverse(ephrase,true)->c = relfreq; 
-	  schwarz.traverse(fphrase,true)->c=*blau; //und befüllen ihn mit informationen
-	  
-	  //cout << "content im blaubaum "<< blau.traverse(ephrase)->c << endl;
-     }
+		while(getline(in,line)){ 
+			stringstream ist(line);
+
+			double relfreq;
+			vector<uint> ephrase, fphrase;
+
+			ist >> relfreq >> token >>token; // token für "#" ACHTUNG das zweite Einlesen ist nur für die alten Trainingsdaten,da gibts relfreq zweimal
+
+			while(ist>>token && token != "#"){
+				fphrase.push_back(flex.getWord_or_add(token));
+			}
+			while(ist>>token){
+				ephrase.push_back(elex.getWord_or_add(token));
+			}
+
+			/* PTree<double>* blau=new PTree<double>(); //wir alloquieren speicher für den blauBaum 
+			blau->traverse(ephrase,true)->c = relfreq; 
+			schwarz.traverse(fphrase,true)->c=*blau; //und befüllen ihn mit informationen
+
+			//cout << "content im blaubaum "<< blau.traverse(ephrase)->c << endl;*/
+			schwarz.traverse(fphrase,true)->c.traverse(ephrase,true)->c = relfreq;
+			
+			//cout << "content im blaubaum " << schwarz.traverse(fphrase)->c.traverse(ephrase)->c << "\n";
+		}
+		
      
      aStar::Suchalgorithmus(argv[2],&schwarz,&elex,&flex);
      
