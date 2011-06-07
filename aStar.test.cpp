cc9edeff371fe9a554ae4d0b8b0ef6d64cea048f
Niklas Fischer <niklasfi@gmail.com>
2011-06-07 08:48:52 +0200

    Test hat jetzt ein Lexicon

--- aStar.test.cpp
+++ aStar.test.cpp
@@ -5,15 +5,29 @@
 #include "aStar.h"
 #include "HypothesisNode.h"
 #include "PartialTranslation.h"
+#include "lexicon.h"
+#include "language.h"
 
 bool test1(){
+	std::vector<uint>w(6); //words
+	Lexicon elex(english);
+	
+	aStar::elex = &elex;
+	
+	w[0] = elex.add("null");
+	w[1] = elex.add("eins");
+	w[2] = elex.add("zwei");
+	w[3] = elex.add("drei");
+	w[4] = elex.add("vier");
+	w[5] = elex.add("fünf");
+	
 	std::vector<HypothesisNode> h(4);
 		
 	PartialTranslation 
-		t1(3,std::vector<uint>{1},&h[1]),
-		t2(4,std::vector<uint>{2},&h[2]),
-		t3(2,std::vector<uint>{3},&h[2]),
-		t4(1,std::vector<uint>{4,5},&h[3]);
+		t1(3,std::vector<uint>{w[1]},&h[1]),
+		t2(4,std::vector<uint>{w[2]},&h[2]),
+		t3(2,std::vector<uint>{w[3]},&h[2]),
+		t4(1,std::vector<uint>{w[4],w[5]},&h[3]);
 		
 	h[0].add_Translation(&t1);
 	h[0].add_Translation(&t3);
@@ -23,13 +37,14 @@ bool test1(){
 	h[2].add_Translation(&t3);
 		
 	aStar a(h);
-		
+	
 	a.search();
 		
 	return true;
 }
 
 int main(){
-	assert(test1);
+	assert(test1());
 	
+	assert(false);
 }
