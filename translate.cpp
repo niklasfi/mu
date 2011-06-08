e7d1fa1a4218868c95eb079474155489b940e497
Niklas Fischer <niklasfi@gmail.com>
2011-06-08 23:51:30 +0200

    pwsr optionen nun verfügbar. mit pwr bleu von 0.19!

--- translate.cpp
+++ translate.cpp
@@ -15,25 +15,24 @@ using namespace std;
 int main (int argc, char* argv[]){
      Lexicon elex;
      Lexicon flex;
-	 bool phrasePenalty, wordPenalty, singleCountBit, stRatio; //stRatio =Source-Target-Ratio 
+	 bool phrasePenalty = false, wordPenalty = false, singleCountBit = false, stRatio = false; //stRatio =Source-Target-Ratio 
 
      PTree<PTree<double>> schwarz;
 
 		if(argc<3){
-		cout<<"usage: template.exe <phrasentabelle> <quelltext>\n";
+		cout<<"usage: template.exe <phrasentabelle> <quelltext> [pwsr]\n";
 		exit(1);
 	}
 
 	for(int i=3; i<argc;i++){
-		switch ((int)argv[i]){
-			//case "p":
-			case 1 : phrasePenalty=1; break;
-			//case "w": 
-			case 2 : wordPenalty = 1; break;
-			//case "s": 
-			case 3 : singleCountBit = 1; break;
-			//case "r": 
-			case 4 : stRatio = 1; break;
+		std::string in = argv[i];
+		if     (in == "p") phrasePenalty=1;
+		else if(in == "w") wordPenalty = 1;
+		else if(in == "s") singleCountBit = 1;
+		else if(in == "r") stRatio = 1;
+		else{
+			cout << "invalid argument!\n";
+			exit (1);
 		}
 	}
 
@@ -54,7 +53,7 @@ int main (int argc, char* argv[]){
 			while(ist>>token && token != "#"){
 				fphrase.push_back(flex.getWord_or_add(token));
 			}
-			while(ist>>token){
+			while(ist>>token && token != "#"){
 				ephrase.push_back(elex.getWord_or_add(token));
 			}
 			if(phrasePenalty) relfreq+=1;
@@ -72,6 +71,4 @@ int main (int argc, char* argv[]){
 
      aStar::set_max_SentenceTranslation(1);
      aStar::Suchalgorithmus(argv[2],&schwarz,&elex,&flex);
-
-     cerr << "Zuende" << endl;
 }
