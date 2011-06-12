070af2a98625f751e0e622a6905d790b225791b7
Niklas Fischer <niklasfi@gmail.com>
2011-06-13 01:08:24 +0200

    jetzt compiliert sie auch, und es müssen noch die operatoren überladen werden

--- cost.cpp
+++ cost.cpp
@@ -7,7 +7,7 @@ std::vector<int> Cost::lookup;
 typedef unsigned int uint;
 
 void Cost::select(std::vector<Model> ms){
-	if(!lookup.empty()) assert(false) //man darf sich nur einmal entscheiden, welche Modelle man will!
+	if(!lookup.empty()) assert(false); //man darf sich nur einmal entscheiden, welche Modelle man will!
 	
 	lookup.resize(modelCount,-1);
 	
@@ -18,52 +18,53 @@ void Cost::select(std::vector<Model> ms){
 			lookup[i] = nextPos++;
 	}
 	
-	scale = std::vector(modelCount,1);
+	scale = std::vector<double>(modelCount,1);
 }
 
-void setScale(Model m, double v){
+void Cost::setScale(Model m, double v){
 	scale[m] = v;
 }
 
-void setScale(std::vector <std::pair<Model, double>> pairVec){
+void Cost::setScale(std::vector <std::pair<Model, double>> pairVec){
 	for(uint i = 0; i < pairVec.size(); i++)
 		setScale(pairVec[i].first, pairVec[i].second);
 }
 
-double cost(){
+double Cost::cost() const {
 	return totalize();
 }
 
-double& cost(Model m){
+double Cost::cost(Model m){
+	if (lookup[m] == -1)	return 0;
 	return modelCosts[lookup[m]] * scale[m];
 }
 
-std::vector<double> cost(std::vector<model> ms){
+std::vector<double> Cost::cost(std::vector<Model> ms){
 	std::vector<double> res;
 	for(uint i = 0; i < ms.size(); i++)
 		res.push_back(cost(ms[i]));
 }
 
-Cost& cost(Model m, double v){
+Cost& Cost::cost(Model m, double v){
 	if(lookup[m]!=-1)
-		cost(m) = v;
+		modelCosts[lookup[m]]=v;
 	return *this;
 }
 
-Cost& cost(std::vector<std::pair<Model,double>> mp){
+Cost& Cost::cost(std::vector<std::pair<Model,double>> mp){
 	for(uint i = 0; i < mp.size(); i++)
-		cost(mp[i].first) = mp[i].second;
+		cost(mp[i].first, mp[i].second);
 	return *this;
 }
 
-double totalize(){
+double Cost::totalize() const {
 	double total = 0;
 	for(uint i = 0; i < lookup.size(); i++)
-		if(lookup[i] != -1) total += cost(i);
+		total += cost(Model(i)); //falls model nicht existiert rechnet er + 0
 	return total;
 }
 
-double calc(
+double Cost::calc(
 	double rel_freq_f,
 	double rel_freq_e,
 	const std::vector<unsigned int>& f_phrase,
@@ -71,7 +72,7 @@ double calc(
 	unsigned int f_singlecount,
 	unsigned int e_singlecount,
 	double source_to_target,
-	double target_to_source
+	double target_to_source,
 	double unigram
 ){
 	return 
@@ -82,6 +83,6 @@ double calc(
 	calc_phrase_penalty().
 	calc_word_penalty(e_phrase).
 	calc_single_count_bit(f_singlecount, e_singlecount).
-	calc_source_to_target_ratio{f_phrase, e_phrase).
-	calc_unigram_language_model(unigram);
+	calc_source_to_target_ratio(f_phrase, e_phrase).
+	calc_unigram_language_model(unigram).totalize();
 }
--- cost.h
+++ cost.h
@@ -16,8 +16,13 @@ class Cost{
 		single_count_bit,
 		source_to_target_ratio,
 		unigram_language_model
-	}
+	};
 	static const unsigned int modelCount = 9; //gibt an, wie viele Modelle verfügt werden können
+	
+	//Konstruktor
+	Cost(double val = 1.0/0.0){
+		modelCosts = std::vector< double > (scale.size(),val);
+	}
 	
 	static void select(std::vector<Model>); //select the models you want to use
 
@@ -27,13 +32,13 @@ class Cost{
 	static void setScale(std::vector< std::pair<Model,double> >);
 
 	std::vector<double> modelCosts; //speichert die werte der einzelnen Modelle
-	double cost(); //same as totalize
-	private: double& cost(Model); //get (or set, as this is an lvalue) cost for model - geht kaputt, wenn das Model nicht gesetzt ist
-	public: std::vector<double> cost(std::vector<Model>); //get vector of costs for multiple models
+	double cost() const; //same as totalize
+	double cost(Model); //get (or set, as this is an lvalue) cost for model - geht kaputt, wenn das Model nicht gesetzt ist
+	std::vector<double> cost(std::vector<Model>); //get vector of costs for multiple models
 	Cost& cost(Model, double); //alternative way of setting the costs for a model
 	Cost& cost(std::vector<std::pair<Model,double>>); //set multiple costs
 
-	double totalize(); //calculates total costs given the current scaling
+	double totalize() const; //calculates total costs given the current scaling
 	
 	double calc(
 		double rel_freq_f,
@@ -43,9 +48,9 @@ class Cost{
 		unsigned int f_singlecount,
 		unsigned int e_singlecount,
 		double source_to_target,
-		double target_to_source
+		double target_to_source,
 		double unigram
-	) //sets the penalties for all Models
+	); //sets the penalties for all Models
 	
 	Cost& calc_source_to_target_phrase(double rel_freq_f){
 		return cost(source_to_target_phrase,rel_freq_f);
@@ -54,28 +59,37 @@ class Cost{
 		return cost(target_to_source_phrase,rel_freq_e);
 	}
 	Cost& calc_source_to_target_unigram(double source_to_target){
-		return cost(source_to_target_word,source_to_target);
+		return cost(source_to_target_unigram,source_to_target);
 	}
 	Cost& calc_target_to_source_unigram(double target_to_source){
-		return cost(target_to_source_word,target_to_source);
+		return cost(target_to_source_unigram,target_to_source);
 	}
-	calc_phrase_penalty(){
+	Cost& calc_phrase_penalty(){
 		return cost(phrase_penalty,1);
 	}
-	calc_word_penalty(const std::vector<unsigned int>& e_phrase){
+	Cost& calc_word_penalty(const std::vector<unsigned int>& e_phrase){
 		return cost(word_penalty,e_phrase.size());
 	}
-	calc_single_count_bit(unsigned int f_singlecount, unsigned int e_singlecount){
-		return cost(single_count_bit, f_singlecount > 1 && e_singlecount > 1 ? 1 : 0)
+	Cost& calc_single_count_bit(unsigned int f_singlecount, unsigned int e_singlecount){
+		return cost(single_count_bit, f_singlecount > 1 && e_singlecount > 1 ? 1 : 0);
 	}
-	calc_source_to_target_ratio{const std::vector<unsigned int>& f_phrase, const std::vector<unsigned int>& e_phrase){
-		return cost(source_to_target_ratio,(1.0*f_phrase.size())/e_phrase.size())
+	Cost& calc_source_to_target_ratio(const std::vector<unsigned int>& f_phrase, const std::vector<unsigned int>& e_phrase){
+		return cost(source_to_target_ratio,(1.0*f_phrase.size())/e_phrase.size());
 	}
-	calc_unigram_language_model(double unigram){
+	Cost& calc_unigram_language_model(double unigram){
 		return cost(unigram_language_model, unigram);
 	}
+	
+	Cost& operator+=(const Cost& c);
+	Cost operator+ (const Cost& c);
+	
+	Cost& operator-=(const Cost& c);
+	cost& operator- (const Cost& c);
+	
+	bool operator==(const Cost& c);
+	bool operator!=(const Cost& c);
 
 	private:
 	
-	static vector<int> lookup; // falls das modell gesetzt ist, dann wird hier die stelle im vektor des Modells angegeben sonst -1 
-}
+	static std::vector<int> lookup; // falls das modell gesetzt ist, dann wird hier die stelle im vektor des Modells angegeben sonst -1 
+};
