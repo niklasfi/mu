f194038b5e695a547f9317a253c43ad699594e62
Niklas Fischer <niklasfi@gmail.com>
2011-06-29 21:25:29 +0200

    ein kleines phraseextract mit ca. 500000 zeilen

--- optimize.cpp
+++ optimize.cpp
@@ -6,7 +6,7 @@
 #include "readtable.cpp"
 #include "sentenceinfo.h"
 #include "global.h"
-//#include "mert.cpp"
+#include "mert.cpp"
 #include "simplex.cpp"
 
 using namespace std;
@@ -16,33 +16,33 @@ int main (int argc, char* argv[]) {
 
 	int mNumber = 1;
 	vector <double> skalierung(9,0);
-	std::vector<std::pair<std::vector<uint>, std::vector<SentenceInfo> > > nBestLists;
+	std::vector<std::pair<std::vector<uint>, std::vector<SentenceInfo> > >* nBestLists = new std::vector<std::pair<std::vector<uint>, std::vector<SentenceInfo> > >;
 
 	Lexicon elex;
 	Lexicon flex;
-	PTree<PTree<Cost>> schwarz;
+	PTree<PTree<Cost>>* schwarz = new PTree<PTree<Cost>>;
 
-	readTable(schwarz,flex,elex,"training/test_singlewordextract",2,10);
+	readTable(*schwarz,flex,elex,"training/phraseextract_sorted",2,10);
 	
 	aStar::set_max_SentenceTranslation(10);
-    aStar::Suchalgorithmus("training/franzesatz",&schwarz,&elex,&flex,nBestLists);
+    aStar::Suchalgorithmus("training/f-test",schwarz,&elex,&flex,*nBestLists);
 
 	ifstream guess("training/englischsatz");
 	string line;
 	
-	for(int j=0;j<nBestLists.size();j++){
+	for(unsigned int j=0;j<nBestLists->size();j++){
 		getline(guess,line);
 			string token;
 			istringstream s_str(line);
 
 			while(s_str >> token) {
-				nBestLists[j].first.push_back(elex.getWord_or_add(token));
+				(*nBestLists)[j].first.push_back(elex.getWord_or_add(token));
 			}
 	}
 
 
-	skalierung=DownhillSimplex(nBestLists);
-	for(int i=0; i<9;i++) cout<<skalierung[i]<<endl;
-	//mert(nBestLists,mNumber);
+	/*skalierung=DownhillSimplex (nBestLists);
+	for(unsigned int i=0; i<9;i++) cout<<skalierung[i]<<endl;*/
+	mert(*nBestLists,mNumber);
 	return 0;
 }
--- sentenceinfo.h
+++ sentenceinfo.h
@@ -6,5 +6,6 @@ struct SentenceInfo{
 	Cost cost;
 	std::vector< std::pair <unsigned int,unsigned int> >* bleu; // matchcount, gramcount
 
+	SentenceInfo():bleu(0){}
 	SentenceInfo(std::vector<unsigned int> sentence, Cost cost, std::vector< std::pair <unsigned int,unsigned int> >* bleu=0): sentence(sentence), cost(cost), bleu(bleu){}
 };
