7658fb5177710cc0d7c1c9486d963cda6b91d60f
Valentina <valen_gerber@yahoo.de>
2011-06-29 19:14:41 +0200

    senteneinfo Null-Pointer

--- sentenceinfo.h
+++ sentenceinfo.h
@@ -5,4 +5,10 @@ struct SentenceInfo{
 	std::vector<uint> sentence;
 	Cost cost;
 	std::vector< std::pair <uint,uint> >* bleu; // matchcount, gramcount
+
+	SentenceInfo& operator=(const SentenceInfo& s){
+	sentence=s.sentence;
+	cost=s.cost;
+	bleu=s.bleu;
+	}
 };
