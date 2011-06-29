76b6864ca2c90765dde8e47027862df402ea5446
Elisa <elifri@ubuntu.(none)>
2011-06-29 19:10:33 +0200

    die Sentenceinfo hat jetzt einen Konstruktor,m der *bleu auf NULL setzt und der wurde in aStar aktualisiert

--- sentenceinfo.h
+++ sentenceinfo.h
@@ -5,4 +5,6 @@ struct SentenceInfo{
 	std::vector<uint> sentence;
 	Cost cost;
 	std::vector< std::pair <uint,uint> >* bleu; // matchcount, gramcount
+	
+	SentenceInfo(vector<uint> sentence, Cost cost, std::vector< std::pair <uint,uint> >* bleu=NULL): sentence(sentence), cost(cost), bleu(bleu){}
 };
