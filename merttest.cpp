8aa987a0e32faeaf419964b2a1041e76e96732c5
Elisa <elifri@ubuntu.(none)>
2011-07-07 00:01:20 +0200

    habe meine tests(randomPermutation und selectedModels) eingefügt

--- merttest.cpp
+++ merttest.cpp
@@ -1,12 +1,16 @@
 #include "mert.cpp"
 #include <vector>
+#include <list>
 #include "global.h"
 #include "sentenceinfo.h"
 #include "cost.h"
+#include <assert.h>
 
-int main () {
+using namespace std;
 
-	std::vector<std::pair<std::vector<uint>, std::vector<SentenceInfo> > > nBestList;
+
+bool test_mert(){
+	vector<std::pair<std::vector<uint>, std::vector<SentenceInfo> > > nBestList;
 
 	std::pair<std::vector<uint>, std::vector<SentenceInfo> > pair_tmp;
 	std::vector<SentenceInfo> hypothesis_tmp;
@@ -52,6 +56,37 @@ int main () {
 	nBestList.push_back(pair_tmp);
 
 	mert(nBestList,2); //Mert ausführen
+	return true;
+}
 
+bool test_RandomPermutation(){
+	std::list<int> Zahlen=randomPermutation(10);
+	
+	//überprüfe, ob jede Zahl da ist
+	for (int i=0; i<10; i++){
+		bool found_i=false;
+		for (std::list<int>::iterator it=Zahlen.begin() ; it != Zahlen.end(); it++ )
+			found_i= ((*it) == i) || found_i;
+		assert(found_i);
+	}
+	assert(Zahlen.size() == 10);
+	return true;
+}
+
+bool test_selectedModel(){
+	vector<Cost::Model> models = selectModels(3);
+	assert(models[0] == Cost::source_to_target_phrase);
+	assert(models[2] == Cost::source_to_target_unigram);
+	return true;
+}
+
+bool test_addNewHypothesis(){
+	return true;
+}
+
+int main () {
+	assert(test_mert());
+	assert(test_RandomPermutation());
+	assert(test_selectedModel());
 	return 0;
 }
